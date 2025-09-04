c66c108903f5e8eabcb1e5b602a29b4afde2b7e0
santy <sandresrodriguezmo@gmail.com>
2025-09-04 12:54:50 +0200

    initian algorimt

---
+++ algoritmo simple/listas enlazadas/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef struct link{
+
+    int datos;
+    struct nodo; //la estructura usa recursividad
+
+}link;
+
+struct lista *l; /* declaración */
+l = NULL; /* inicialización */
