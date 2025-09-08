795808897daea3eed7439f16c42f0cf3b3b17466
santy <sandresrodriguezmo@gmail.com>
2025-09-08 21:55:21 +0200

    double linked-list init

---
+++ listas enlazadas/lista enlazada bidirecciónal/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//definicion de la estructura del nodo
+struct Node
+{
+    int data; // Valor almacenado en el nodo 
+    struct Node* next; //puntero al siguiente nodo
+    struct Node* prev; //puntero al anterior nodo
+};
+
+//operacion basica para crear un nodo
+struct Node* createNode(int value)
+{
+
+    //asignar memoria para crear un nuevo nodo
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    if (newNode == NULL)
+    {   
+        printf("Memory allocator failed");
+        exit(1);
+    }
+
+    newNode->data = value; //asignar el valor (value) al campo data de la estructura Node
+    newNode->next = NULL; //asignar valor null al puntero del siguiente nodo
+    newNode->prev = newNode;
+
+    //retorna el puntero del nuevo nodo creado
+    return newNode;
+
+};
+
+int main()
+{
+    struct Node* node = createNode(42);
+
+    // Verificar que el nodo se creó correctamente imprimiendo su valor
+    printf("Nodo creado con valor: %d\n", node->data);
+
+    // Liberar la memoria asignada al nodo
+    free(node);
+
+    return 0;
+}
+
+
